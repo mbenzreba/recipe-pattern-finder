cfa565aa08fa5420a0b1a7411abb10b466972594
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-02-26 13:51:14 -0500

    add: empty POSPatternReporter

---
+++ src/main/java/com/mbenzreba/RecipePatternFinder/POSPatternReporter.java
@@ -0,0 +1,16 @@
+package com.mbenzreba.RecipePatternFinder;
+
+
+
+/**
+ * Used to streamline the analysis of phrase-of-speech patterns found in recipes.
+ * 
+ * @author Mohamed Benzreba
+ */
+public class POSPatternReporter {
+    
+    public POSPatternReporter(String resource)
+    {
+        //...
+    }
+}
