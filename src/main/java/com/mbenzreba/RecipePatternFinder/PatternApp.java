09901165cc3653720654e1f70cb0ae57955e475a
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-05 14:21:17 -0500

    add: new pattern to match

--- src/main/java/com/mbenzreba/RecipePatternFinder/PatternApp.java
+++ src/main/java/com/mbenzreba/RecipePatternFinder/PatternApp.java
@@ -4,8 +4,8 @@ public class PatternApp
 {
     public static void main(String[] args)
     {
-        POSPatternReporter reporter = new POSPatternReporter("peruvian_chicken");
-        String[] pat = {"IN", "DT", "NN"};
+        POSPatternReporter reporter = new POSPatternReporter("recipes_train");
+        String[] pat = {"NN", "DT", "NN"};
         int times = reporter.findPattern(pat);
         System.out.println("\nPattern occurs " + times + " times");
     }
