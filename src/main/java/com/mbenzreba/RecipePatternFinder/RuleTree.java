e6ff053f39045426ef4167ca7f5e09c778d32c9e
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-19 14:25:40 -0400

    add basic framework for initializing nodes for use in a RuleTree, so that WordTrees can be later corrected

---
+++ src/main/java/com/mbenzreba/RecipePatternFinder/RuleTree.java
@@ -0,0 +1,61 @@
+package com.mbenzreba.RecipePatternFinder;
+
+
+/**
+ * @author Mohamed Benzreba
+ */
+public abstract class RuleTree {
+
+    protected RuleTreeNode _root;
+
+    protected RuleTree(String rule) {
+        this._initializeNode(rule);
+    }
+
+    protected RuleTreeNode _initializeNode(String rule) {
+        int currLevel = 1;
+
+        // TODO: Set up a loop to process every single command found in the rule
+        // This method calls the RuleInterpreter -- the interpreter then passes an appropriate node back
+
+        // TODO: return something good
+        return new RuleTreeNode("EMPTY-TREE");
+    }
+
+    protected abstract void _cleanAnnotations();
+
+
+    /**
+     * @author Mohamed Benzreba
+     */
+    protected class RuleTreeNode {
+
+        /** Phrase of speech of the node */
+        protected String _pos;
+        /** Value; note, NOT 'word', since value of the node can now be eitehr a word, or target, etc. */
+        protected String _value;
+
+        /** Structural pattern of this node's children
+         * @see com.mbenzreba.RecipePatternFinder.types.NodeChildrenPattern
+         */
+        protected NodeChildrenPattern _ncp;
+
+        /** Type of the leaf of this node
+         * @see com.mbenzreba.RecipePatternFinder.types.LeafType
+         */
+        protected LeafType _leafType;
+
+        public RuleTreeNode(String pos) {
+            this._pos = pos;
+            this._value = null;
+            this._ncp = NodeChildrenPattern.NONE;
+            this._leafType = LeafType.NONE;
+        }
+
+    }
+    
+}
+
+
+
+
---
+++ src/main/java/com/mbenzreba/RecipePatternFinder/types.java
@@ -0,0 +1,33 @@
+package com.mbenzreba.RecipePatternFinder;
+
+
+/**
+ * @author Mohamed Benzreba
+ */
+enum NodeChildrenPattern {
+    /** Children of this node must be arranged in the same way as this node's children */
+    SPECIFIC,
+    /** Children of this node must exist in this order, but can accept some variance (e.g. some other children 
+     * exist in between the requisite nodes) */
+    GENERAL,
+    /** Examine the leaves extending from this node, no matter how far down they might be */
+    LEAF,
+    /** This node has no requisite children */
+    NONE,
+}
+
+
+
+/**
+ * @author Mohamed Benzreba
+ */
+enum LeafType {
+    /** The 'pos' and 'word' values of the leaf must match exactly to the compared node */
+    SPECIFIC,
+    /** The leaf fulfills a target that the rule defines */
+    TARGET,
+    /** The 'pos' of the leaf can be variable */
+    ANY,
+    /** There is no leaf */
+    NONE,
+}
