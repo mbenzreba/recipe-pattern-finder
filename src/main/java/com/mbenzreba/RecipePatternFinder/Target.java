440dc39fe1b3aa23a93fc5055c02dcbc151d305b
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-19 14:27:06 -0400

    add basic constructors

--- src/main/java/com/mbenzreba/RecipePatternFinder/Target.java
+++ src/main/java/com/mbenzreba/RecipePatternFinder/Target.java
@@ -8,4 +8,19 @@ final class Target
     private boolean _isFulfilled;
     private ArrayList<String> _values;
     private int _nextFix;
+
+    public Target(String name)
+    {
+        this._name = name;
+        this._isFulfilled = false;
+        this._values = new ArrayList<String>();
+    }
+
+    public Target(String name, int fixLine)
+    {
+        this._name = name;
+        this._isFulfilled = false;
+        this._values = new ArrayList<String>();
+        this._nextFix = fixLine;
+    }
 }
