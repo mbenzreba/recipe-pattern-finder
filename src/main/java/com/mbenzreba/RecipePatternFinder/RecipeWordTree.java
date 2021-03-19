e01bf84c5903666770be31352fadf56ab951ede0
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-19 14:28:04 -0400

    add basic fulfill(), for fulfilling targets defined by the Arborist

--- src/main/java/com/mbenzreba/RecipePatternFinder/RecipeWordTree.java
+++ src/main/java/com/mbenzreba/RecipePatternFinder/RecipeWordTree.java
@@ -1,11 +1,34 @@
 package com.mbenzreba.RecipePatternFinder;
 
+
+import java.io.InputStream;
+// Java imports
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
 public class RecipeWordTree extends WordTree 
 {
 
+
+    /**
+     * 
+     * @param sentence
+     */
     public RecipeWordTree(String sentence) 
     {
+        // extends WordTree
         super(sentence);
+    }
+
+
+    public boolean fulfill(Target t)
+    {
+        // Try to fix it
+        // TODO
+        return true;
     }
     
 }
